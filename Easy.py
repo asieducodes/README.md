688fc66ac15ddca3a85644bdf507fea0c1aa2792
Seth Smart <asiedusethosei@gmail.com>
2025-05-10 07:42:39 -0700

    Modified

---
+++ Easy.py
@@ -0,0 +1,11 @@
+print("Hello World")
+class All:
+    def __init__(self):
+       # self.username=username
+        pass
+    def greetings(username):
+        print(f"Good morning, {username}")
+    
+person=All
+person.greetings("Asamoah")
+
